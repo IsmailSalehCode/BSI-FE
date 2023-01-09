8558b8a5fab63fdbb9e54085ae8b460dcad1fbb9
Ismail Saleh <centriu78@gmail.com>
2023-01-09 23:50:00 +0200

    p

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,37 +1,35 @@
 <template>
   <v-footer theme="dark" absolute app bottom>
     <v-container>
-      <v-row style="text-align: center">
-        <v-col cols="12">
+      <v-col cols="12">
+        <v-row style="text-align: center" align="center" justify="center">
           <span>
             &#169; {{ new Date().getFullYear() }} -
             <strong>Blue Sky Invest Ltd.</strong> All rights reserved.
           </span>
-        </v-col>
-      </v-row>
+        </v-row>
+      </v-col>
       <v-divider></v-divider>
-      <v-row>
-        <v-col cols="12">
-          <v-row align="center" justify="center" style="text-align: center">
-            <v-col cols="6">
-              <v-btn size="small" variant="flat" @click="openTermsAndConditions"
-                >Terms & Conditions</v-btn
+      <v-col cols="12">
+        <v-row align="center" justify="center" style="text-align: center">
+          <v-col cols="6">
+            <v-btn size="small" variant="flat" @click="openTermsAndConditions"
+              >Terms & Conditions</v-btn
+            >
+          </v-col>
+          <v-col cols="6">
+            <div v-for="item in contactItems" :key="item.text">
+              <v-btn
+                size="small"
+                variant="flat"
+                :prepend-icon="item.icon"
+                :href="item.path"
+                >{{ item.text }}</v-btn
               >
-            </v-col>
-            <v-col cols="6">
-              <div v-for="item in contactItems" :key="item.text">
-                <v-btn
-                  size="small"
-                  variant="flat"
-                  :prepend-icon="item.icon"
-                  :href="item.path"
-                  >{{ item.text }}</v-btn
-                >
-              </div>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
+            </div>
+          </v-col>
+        </v-row>
+      </v-col>
     </v-container>
   </v-footer>
 </template>
