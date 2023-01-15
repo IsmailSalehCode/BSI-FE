1be8db21d89d481de4b194cb59a6e927631c97d1
Ismail Saleh <centriu78@gmail.com>
2023-01-16 00:32:39 +0200

    a

--- src/components/DialogOpenedPhoto.vue
+++ src/components/DialogOpenedPhoto.vue
@@ -3,12 +3,18 @@
     <v-card>
       <v-toolbar density="compact">
         <v-spacer></v-spacer>
-        <v-btn icon @click="close">
-          <v-icon>mdi-close</v-icon>
+        <v-btn icon color="red" @click="close">
+          <v-icon>mdi-window-close</v-icon>
         </v-btn>
       </v-toolbar>
       <v-img :src="currentPhoto.url"></v-img>
       <!-- <v-card-text> Collection: {{ collection.title }} </v-card-text> -->
+      <v-card-actions class="pt-0 mt-0">
+        <v-row no-gutters class="pt-0 mt-0">
+          <v-col cols="6"><v-btn size="small" block>&ltcc;</v-btn></v-col>
+          <v-col cols="6"><v-btn size="small" block>&gtcc;</v-btn></v-col>
+        </v-row>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
