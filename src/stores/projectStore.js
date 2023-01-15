d62060ac332077f5bd72485ef472fdbecfdae3f0
Ismail Saleh <centriu78@gmail.com>
2023-01-15 19:17:22 +0200

    a

--- src/stores/projectStore.js
+++ src/stores/projectStore.js
@@ -12,6 +12,88 @@ export const useProjectStore = defineStore("project", {
             photo: "https://imgur.com/2uiEAj0.jpg",
             order: 1,
           },
+          photos: [
+            {
+              id: 1,
+              taken: null,
+              url: "https://imgur.com/4XFzUUk.jpg",
+            },
+            {
+              id: 2,
+              taken: null,
+              url: "https://imgur.com/4bakYwJ.jpg",
+            },
+            {
+              id: 3,
+              taken: null,
+              url: "https://imgur.com/d3wME5U.jpg",
+            },
+            {
+              id: 4,
+              taken: null,
+              url: "https://imgur.com/8vZWUwm.jpg",
+            },
+            {
+              id: 5,
+              taken: null,
+              url: "https://imgur.com/ZwzKEd2.jpg",
+            },
+            {
+              id: 6,
+              taken: null,
+              url: "https://imgur.com/ESD8LLi.jpg",
+            },
+            {
+              id: 7,
+              taken: null,
+              url: "https://imgur.com/V6fLvE8.jpg",
+            },
+            {
+              id: 8,
+              taken: null,
+              url: "https://imgur.com/rUIH0rb.jpg",
+            },
+            {
+              id: 9,
+              taken: null,
+              url: "https://imgur.com/8w0bBKE.jpg",
+            },
+            {
+              id: 10,
+              taken: null,
+              url: "https://imgur.com/TR69ivc.jpg",
+            },
+            {
+              id: 11,
+              taken: null,
+              url: "https://imgur.com/LLSJTQj.jpg",
+            },
+            {
+              id: 12,
+              taken: null,
+              url: "https://imgur.com/6t38qW7.jpg",
+            },
+            {
+              id: 13,
+              taken: null,
+              url: "https://imgur.com/kt5FU56.jpg",
+            },
+            {
+              id: 14,
+              taken: null,
+              url: "https://imgur.com/AkcRcp6.jpg",
+            },
+            {
+              id: 15,
+              taken: null,
+              url: "https://imgur.com/oQ8PP0m.jpg",
+            },
+            {
+              id: 16,
+              taken: null,
+              url: "https://imgur.com/2uiEAj0.jpg",
+            },
+          ],
         },
         {
           title: "Pan Ocean",
@@ -21,6 +103,23 @@ export const useProjectStore = defineStore("project", {
             photo: "https://i.imgur.com/O99G249.jpg",
             order: 2,
           },
+          photos: [
+            {
+              id: 1,
+              taken: null,
+              url: "https://i.imgur.com/YtKRSN1.jpg",
+            },
+            {
+              id: 2,
+              taken: null,
+              url: "https://i.imgur.com/xCM1XXm.jpg",
+            },
+            {
+              id: 3,
+              taken: null,
+              url: "https://i.imgur.com/O99G249.jpg",
+            },
+          ],
         },
         {
           title: "Balkan Pearl",
@@ -30,6 +129,23 @@ export const useProjectStore = defineStore("project", {
             photo: "https://i.imgur.com/ndejd2u.jpg",
             order: 3,
           },
+          photos: [
+            {
+              id: 1,
+              taken: null,
+              url: "https://i.imgur.com/QdEm07B.jpg",
+            },
+            {
+              id: 2,
+              taken: null,
+              url: "https://i.imgur.com/R9CZm3w.jpg",
+            },
+            {
+              id: 3,
+              taken: null,
+              url: "https://i.imgur.com/ndejd2u.jpg",
+            },
+          ],
         },
         {
           title: "Acres Invest",
