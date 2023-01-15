b18269a0c070c064d3e4ea4194bdeb38cf2a4281
Ismail Saleh <centriu78@gmail.com>
2023-01-15 15:05:05 +0200

    ay

--- src/sections/CompanyAbout.vue
+++ src/sections/CompanyAbout.vue
@@ -51,6 +51,21 @@
         </v-col>
         <v-col>
           <v-card elevation="6" class="mx-auto" max-width="500" min-width="300">
+            <v-card-title>Safety Commitment</v-card-title>
+            <v-card-text>
+              We are dedicated to working safely and strive to achieve “Zero
+              Accidents” on every project we build. In addition to enforcing
+              <a
+                href="https://www.osha.gov/sites/default/files/publications/osha3021.pdf"
+                target="_blank"
+                >OSHA regulations</a
+              >, we provide ongoing training and leadership to our staff and
+              labor force to ensure a safe work site.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card elevation="6" class="mx-auto" max-width="500" min-width="300">
             <v-card-title>Looking Ahead</v-card-title>
             <v-card-text>
               We also invest in construction projects and collaborate with
@@ -58,7 +73,7 @@
             </v-card-text>
             <v-card-text class="pt-0">
               Have an idea? We'd love to
-              <a href="/contact"><strong>hear from you!</strong></a>
+              <a href="/contact">hear from you!</a>
             </v-card-text>
           </v-card>
         </v-col>
@@ -82,6 +97,9 @@ export default {
 </script>
 
 <style scoped>
+a {
+  font-weight: bold;
+}
 .v-card-text {
   text-indent: 4px;
   text-align: justify;
