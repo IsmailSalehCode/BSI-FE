d4f0de70f4538485af43b1a1916d48be05fda222
Ismail Saleh <centriu78@gmail.com>
2023-01-15 16:33:27 +0200

    a

--- src/components/BtnProjectsMenu.vue
+++ src/components/BtnProjectsMenu.vue
@@ -20,63 +20,15 @@
       </v-list-item>
     </v-list>
   </v-menu>
-  <!-- <v-menu dark rounded="b-xl t-0" offset-y>
-    <template v-slot:activator="{ attrs, on }">
-      <v-btn text dark v-bind="attrs" v-on="on">
-        <b>Projects</b>
-        <v-icon right>mdi-chevron-down</v-icon>
-      </v-btn>
-    </template>
-
-    <v-list v-if="!noProjects">
-      <template v-for="(project, index) in projects" :key="project.title">
-        <v-list-item link :to="project.path" dense>
-          <v-list-item-title>{{ project.title }}</v-list-item-title>
-        </v-list-item>
-
-        <v-divider
-          v-if="project.divider"
-          :key="index"
-          color="white"
-        ></v-divider>
-      </template>
-    </v-list>
-    <v-list v-else>
-      <v-list-item> No Uploads </v-list-item>
-    </v-list>
-  </v-menu> -->
 </template>
 <script>
+import { useProjectStore } from "@/stores/projectStore";
+import { mapState } from "pinia";
 export default {
-  data() {
-    return {
-      projects: [
-        {
-          title: "Acres Invest",
-          path: "/acres-invest-jsc",
-          divider: true,
-        },
-        {
-          title: "Grand Hotel Abeer",
-          path: "/posts/grand-hotel-abeer",
-          divider: true,
-        },
-        {
-          title: "Pan Ocean",
-          path: "/posts/pan-ocean",
-          divider: true,
-        },
-        {
-          title: "Balkan Pearl",
-          path: "/posts/balkan-pearl",
-          divider: false,
-        },
-      ],
-    };
-  },
   computed: {
+    ...mapState(useProjectStore, ["projects"]),
     noProjects() {
-      return this.projects.length == 0;
+      return this.projectStore.length == 0;
     },
   },
 };
--- src/stores/projectStore.js
+++ src/stores/projectStore.js
@@ -1,14 +1,30 @@
 import { defineStore } from "pinia";
 
-export const useContactStore = defineStore("contact", {
-  getters: {
-    projects() {
-      return [
+export const useProjectStore = defineStore("project", {
+  state: () => {
+    return {
+      projects: [
         {
-          title: "",
-          url: "",
+          title: "Acres Invest",
+          path: "/acres-invest-jsc",
+          divider: true,
         },
-      ];
-    },
+        {
+          title: "Grand Hotel Abeer",
+          path: "/posts/grand-hotel-abeer",
+          divider: true,
+        },
+        {
+          title: "Pan Ocean",
+          path: "/posts/pan-ocean",
+          divider: true,
+        },
+        {
+          title: "Balkan Pearl",
+          path: "/posts/balkan-pearl",
+          divider: false,
+        },
+      ],
+    };
   },
 });
