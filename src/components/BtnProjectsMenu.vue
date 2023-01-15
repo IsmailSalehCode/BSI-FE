72fb1e6ab5d37a2f14dec4b790709f8b0d473aaf
Ismail Saleh <centriu78@gmail.com>
2023-01-15 18:56:23 +0200

    a

--- src/components/BtnProjectsMenu.vue
+++ src/components/BtnProjectsMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <v-menu transition="slide-x-transition" theme="dark">
     <template v-slot:activator="{ props }">
-      <v-btn size="small" rounded="0" v-bind="props">Projects</v-btn>
+      <v-btn size="small" rounded="0" v-bind="props" append-icon="mdi-chevron-down">Projects</v-btn>
     </template>
     <v-list density="compact">
       <v-list-item
