ae27e93aa976251eaeebc458b35582101e9d26cf
Ismail Saleh <centriu78@gmail.com>
2024-07-23 20:28:24 +0300

    +

--- src/views/ModularHouses.vue
+++ src/views/ModularHouses.vue
@@ -1,6 +1,12 @@
 <template>
   <v-container>
-    <v-row>{{ $t("hi") }} </v-row>
+    <v-row>
+      <v-col>
+        <h1>Модулярни/сглобяеми къщи</h1>
+        <h2>Срок на изпълнение: 20-30 дни</h2>
+        <h3>Квадратура на етаж: от 50 м<sup>2</sup> до 160 м<sup>2</sup></h3>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script>
