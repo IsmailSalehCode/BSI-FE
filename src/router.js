89093d35394505602d1126927d72fcd56f3d2470
Ismail Saleh <centriu78@gmail.com>
2024-07-23 16:54:52 +0300

    +

--- src/router.js
+++ src/router.js
@@ -12,19 +12,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory("/"),
   routes: routes,
-  // scrollBehavior(to, from, savedPosition) {
-  //   if (to.hash) {
-  //     return {
-  //       el: to.hash,
-  //       behavior: "smooth",
-  //       top: 30,
-  //     };
-  //   }
-  //   if (savedPosition) {
-  //     return savedPosition;
-  //   }
-  //   return { top: 0 };
-  // },
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+        top: 30,
+      };
+    }
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { top: 0 };
+  },
 });
 
 export default router;
