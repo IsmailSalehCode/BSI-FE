a24cfe10666c7f4fac6de0c728f98ea81eeb6790
Ismail Saleh <centriu78@gmail.com>
2024-07-24 19:11:26 +0300

    +

--- src/sections/CompanyAbout.vue
+++ src/sections/CompanyAbout.vue
@@ -4,66 +4,51 @@
       <v-row justify="center">
         <v-col>
           <v-card elevation="6" class="mx-auto" max-width="600" min-width="300">
-            <v-card-title>Welcome</v-card-title>
+            <v-card-title>{{ $t("welcome") }}</v-card-title>
             <v-card-text>
-              In the ever-growing fields of residential, industrial, and public
-              construction, Blue Sky Invest excels at fast, cost-effective and
-              proper project execution.
+              {{ $t("about-p1") }}
             </v-card-text>
             <v-card-text class="pt-0">
-              With <b>over 40 years of expertise at project management</b> the
-              company possesses and follows well-grounded and established
-              principles at every step of the project life cycle, ensuring that
-              every investment our clients make is profitable.
+              <div v-html="aboutp2"></div>
             </v-card-text>
           </v-card>
         </v-col>
         <v-col>
           <v-card elevation="6" class="mx-auto" max-width="500" min-width="300">
-            <v-card-title>By hiring Blue Sky Invest...</v-card-title>
+            <v-card-title>{{ $t("by-hiring-bsi") }}</v-card-title>
             <v-card-text>
-              you not only hire that seasoned professional, but a whole team of
-              subcontractors who are experts in their respective fields and have
-              experience working as a team. Whether it be a new construction
-              project, a renovation, or a remodel, BSI has the leadership and
-              the skills needed to deliver the best services to our clients
-              before the shovel hits the ground.
+              {{ $t("about-p3") }}
             </v-card-text>
             <v-card-text class="pt-0">
-              The benefits of hiring a general contractor increase the overall
-              value of a building as well as provide you, the owner, a relaxed
-              and enjoyable experience before, during, and after the whole
-              process.
+              {{ $t("about-p4") }}
             </v-card-text>
           </v-card>
         </v-col>
         <v-col>
           <v-card elevation="6" class="mx-auto" max-width="500" min-width="300">
-            <v-card-title>Safety Commitment</v-card-title>
+            <v-card-title>{{ $t("safety-commitment") }}</v-card-title>
             <v-card-text>
-              In addition to enforcing
+              {{ $t("in-addition-to-enforcing") }}
               <a
                 class="customLink"
                 href="https://www.mlsp.government.bg/usloviya-na-trud"
                 target="_blank"
-                >government regulations</a
-              >, we provide ongoing training and leadership to our staff and
-              labor force to ensure a safe work site.
+                >{{ $t("government-regulations") }}</a
+              >{{ $t("about-p5") }}
             </v-card-text>
           </v-card>
         </v-col>
         <v-col>
           <v-card elevation="6" class="mx-auto" max-width="500" min-width="300">
-            <v-card-title>Looking Ahead</v-card-title>
+            <v-card-title>{{ $t("la") }}</v-card-title>
             <v-card-text>
-              We also invest in construction projects and collaborate with
-              partners.
+              {{ $t("la-p1") }}
             </v-card-text>
             <v-card-text class="pt-0">
-              Have an idea? We'd love to
-              <router-link to="#contact" class="customLink"
-                >hear from you!</router-link
-              >
+              {{ $t("la-p2") }}
+              <router-link to="#contact" class="customLink">{{
+                $t("hear-from-you")
+              }}</router-link>
             </v-card-text>
           </v-card>
         </v-col>
@@ -71,7 +56,15 @@
     </v-col>
   </v-container>
 </template>
-
+<script>
+export default {
+  computed: {
+    aboutp2() {
+      return this.$t("about-p2");
+    },
+  },
+};
+</script>
 <style>
 a {
   font-weight: bold;
