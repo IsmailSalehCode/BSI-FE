bd375d1fd70757c571afa478380654be92e09153
Ismail Saleh <centriu78@gmail.com>
2024-07-24 15:53:56 +0300

    +

--- src/components/ImgC.vue
+++ src/components/ImgC.vue
@@ -1,5 +1,14 @@
 <template>
-  <v-img :src="src" alt="404.png" />
+  <v-img :src="src" alt="/404.png">
+    <template v-slot:placeholder>
+      <div
+        class="d-flex align-center justify-center"
+        style="height: 200px; width: 200px"
+      >
+        <v-progress-circular color="blue" indeterminate></v-progress-circular>
+      </div>
+    </template>
+  </v-img>
 </template>
 <script>
 export default {
